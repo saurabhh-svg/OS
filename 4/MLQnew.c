ee333ba14f8e8b87a36a88273448a8e905fa55d3
saurabhh-svg <singhsaurabh2901@gmail.com>
2022-04-24 00:20:33 +0530

    commmit

--- 4/MLQnew.c
+++ 4/MLQnew.c
@@ -1,16 +1,15 @@
 #include <stdio.h>
-int main()
+void main()
 {
     // qp is Queue Prioriy, cwt is Cummulative Waiting Time, and ctat is Cummulative Turn Around Time
     int pid[20], bt[20], qp[20], wt[20], tat[20], i, k, n, temp;
     float cwt, ctat;
-    printf(" Enter the number of processes-- - ");
+    printf("Enter the number of processes --- ");
     scanf("%d", &n);
-    for (i = 0; i < n;i++)
+    for (i = 0; i < n; i++)
     {
         pid[i] = i;
-        printf(" Enter the Burst Time of Process % d-- - ", i);
-
+        printf("Enter the Burst Time of Process %d --- ", i);
         scanf("%d", &bt[i]);
         printf("System/User Process (0/1) ? --- ");
         scanf("%d", &qp[i]);
@@ -23,38 +22,44 @@ int main()
                 temp = pid[i];
                 pid[i] = pid[k];
                 pid[k] = temp;
+
                 temp = bt[i];
                 bt[i] = bt[k];
                 bt[k] = temp;
+
                 temp = qp[i];
                 qp[i] = qp[k];
                 qp[k] = temp;
             }
 
     for (i = 0; i < n; i++)
-        for (k = i + 1; k <n; k++)
+        for (k = i + 1; k < n; k++)
         {
-            if (qp[i] == 0  && qp[k] == 0 && bt[i]> bt[k])
+            if (qp[i] == 0 && qp[k] == 0 && bt[i] > bt[k])
             {
                 temp = pid[i];
                 pid[i] = pid[k];
                 pid[k] = temp;
+
                 temp = bt[i];
                 bt[i] = bt[k];
                 bt[k] = temp;
+
                 temp = qp[i];
                 qp[i] = qp[k];
                 qp[k] = temp;
             }
+
             else if (qp[i] == 1 && qp[k] == 1 && pid[i] > pid[k])
             {
                 temp = pid[i];
                 pid[i] = pid[k];
                 pid[k] = temp;
-                temp = bt[i];
 
+                temp = bt[i];
                 bt[i] = bt[k];
                 bt[k] = temp;
+
                 temp = qp[i];
                 qp[i] = qp[k];
                 qp[k] = temp;
@@ -67,14 +72,14 @@ int main()
     {
         wt[i] = wt[i - 1] + bt[i - 1];
         tat[i] = tat[i - 1] + bt[i];
+
         cwt = cwt + wt[i];
         ctat = ctat + tat[i];
     }
-    printf("\nPROCESS\t\t SYSTEM / USER PROCESS \tBURST TIME\tWAITING TIME\tTURNAROUND TIME");
-    for (i = 0; i<n; i++)
-        printf("\n % d \t\t\t\t % d \t\t % d \t\t % d \t\t % d ", pid[i], qp[i], bt[i], wt[i], tat[i]);
-    printf("\nAverage Waiting Time is-- - % f ", cwt / n);
-    printf("\nAverage Turnaround Time is-- - % f ", ctat / n);
+    printf("\nPROCESS\t\t SYSTEM/USER PROCESS \tBURST TIME\tWAITING TIME\tTURNAROUND TIME");
+    for (i = 0; i < n; i++)
+        printf("\n%d \t\t\t\t %d \t\t %d \t\t %d \t\t %d ", pid[i], qp[i], bt[i], wt[i], tat[i]);
 
-    return 0;
+    printf("\nAverage Waiting Time is --- %f", cwt / n);
+    printf("\nAverage Turnaround Time is --- %f", ctat / n);
 }
---
+++ 6/ReplaceCodeImageofParentChild.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char *argv[])
+{
+
+    pid_t pid = fork();
+    if (pid == 0)
+    {
+        printf("Child PID: %d \n", (int)getpid());
+        execlp(argv[1], "arguments", NULL);
+    }
+    else if (pid > 0)
+    {
+        wait(NULL);
+        // printf("Parent resumes from there");
+        printf("Parent PID: %d \n", (int)getpid());
+        execl(argv[2], "some arguments", NULL);
+    }
+}
